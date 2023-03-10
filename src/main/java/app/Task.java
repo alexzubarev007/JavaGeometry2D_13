c13d576021fa2851f674ee2a361c85283ba40086
zubarevat.24 <aleksandrzubarev6@gmail.com>
2023-03-10 12:38:08 +0300

    log works

--- src/main/java/app/Task.java
+++ src/main/java/app/Task.java
@@ -7,6 +7,7 @@ import misc.CoordinateSystem2d;
 import misc.CoordinateSystem2i;
 import misc.Vector2d;
 import misc.Vector2i;
+import panels.PanelLog;
 
 import java.awt.*;
 import java.util.ArrayList;
@@ -85,6 +86,8 @@ public class Task {
     public void addPoint(Vector2d pos, Point.PointSet pointSet) {
         Point newPoint = new Point(pos, pointSet);
         points.add(newPoint);
+        // Добавляем в лог запись информации
+        PanelLog.info("точка " + newPoint + " добавлена в " + newPoint.getSetName());
     }
     /**
      * Клик мыши по пространству задачи
