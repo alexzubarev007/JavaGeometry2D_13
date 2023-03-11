d8efdb5a05e6b835d17a15a64bb6f3e3895e8947
zubarevat.24 <aleksandrzubarev6@gmail.com>
2023-03-11 10:06:16 +0300

    input select works

--- src/main/java/controls/Input.java
+++ src/main/java/controls/Input.java
@@ -22,6 +22,12 @@ public class Input extends GridPanel {
     /**
      * Смещение для красивого отображения текста
      */
+    /**
+     * флаг, помещён ли сейчас фокус на это поле ввода
+     * (модификатор доступа по умолчанию, чтобы был доступен
+     * фабрике InputFactory внутри пакета)
+     */
+    boolean focused = false;
     private static final int LOCAL_PADDING = 8;
     /**
      * Флаг, нужно ли выравнивать текст по центру по вертикали
@@ -59,12 +65,28 @@ public class Input extends GridPanel {
         this.vcentered = vcentered;
         this.textColor = textColor;
     }
-
+    /**
+     * Установить фокус на это поле ввода
+     */
+    public void setFocus() {
+        // снимаем фокус со всех полей ввода
+        InputFactory.defocusAll();
+        // выделяем текущее поле ввода
+        this.focused = true;
+    }
+    /**
+     * Возвращает флаг, установлен ли фокус на это поле ввода
+     *
+     * @return флаг
+     */
+    public boolean isFocused() {
+        return focused;
+    }
 
     /**
      * Метод под рисование в конкретной реализации
      *
-     * @param canvas   область рисования
+     * @param canvas область рисования
      * @param windowCS СК окна
      */
     @Override
@@ -94,7 +116,7 @@ public class Input extends GridPanel {
                 // рисуем линию текста
                 canvas.drawTextLine(line, 0, 0, paint);
                 // если время рисовать курсор
-                if (InputFactory.cursorDraw()) {
+                if (focused && InputFactory.cursorDraw()) {
                     // смещаем область рисования
                     canvas.translate(line.getWidth(), 0);
                     // рисуем его
